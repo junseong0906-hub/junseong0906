505960419e2287b499be9635de9af269bc76ee93
junseong0906-hub <junseong0906@gmail.com>
2026-09-08 17:07:24 +0900

    test

--- main.java
+++ main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        //TEST
         float fahrenheit = sc.nextFloat();
         float celsius = ((float)5 / 9) * (fahrenheit - 32);
 
-        System.out.printf("섭씨 온도: %.1f\n", celsius);
+        System.out.printf("화씨에서 섭씨 온도: %.1f\n", celsius);
 
     }
 }
