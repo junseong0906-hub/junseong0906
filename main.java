3d90be5ac04104a2f6d76f5b1f1589eb10af110e
junseong0906-hub <junseong0906@gmail.com>
2026-09-08 17:26:23 +0900

    배열

--- main.java
+++ main.java
@@ -1,17 +1,18 @@
 
 public class main {
     public static void main(String[] args) {
-        int a = 1;
-        a += 1;
-        System.out.println(a);
+        double arr1[] = { 1.1, 2.3, 4.7, 7.5 }; 
+        double arr2[] = new double[4];
+        arr2[0] = 1.1;
+        arr2[1] = 2.3;
+        arr2[2] = 4.7;
+        arr2[3] = 7.5;        
         
-        float b = 3.2f;
-        b -= a;
-        System.out.println(b);
-
-        b *= a;
-        System.out.println(b);
-
+        System.out.printf("배열크기: %d %d\n", arr1.length, arr2.length);
+        System.out.printf("arr1[0] = %f, arr2[0] = %f\n", arr1[0], arr2[0]);
+        System.out.printf("arr1[1] = %f, arr2[1] = %f\n", arr1[1], arr2[1]);
+        System.out.printf("arr1[2] = %f, arr2[2] = %f\n", arr1[2], arr2[2]);
+        System.out.printf("arr1[3] = %f, arr2[3] = %f\n", arr1[3], arr2[3]);
 
     }
 }
