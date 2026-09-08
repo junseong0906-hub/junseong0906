98d73a9df453d9dc7a33c7be08abd0f09593f02b
junseong0906-hub <junseong0906@gmail.com>
2026-09-08 16:46:58 +0900

    Second commit

--- main.java
+++ main.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        String s = sc.next();
-        System.out.println("문자열: " + s);
 
-        int num = sc.nextInt();
-        System.out.println("정수: " + num);
-        
-        String s1 = sc.nextLine();
-        System.out.println("나머지 문자열: " + s1);
+        float fahrenheit = sc.nextFloat();
+        float celsius = ((float)5 / 9) * (fahrenheit - 32);
+
+        System.out.printf("섭씨 온도: %.1f\n", celsius);
+
     }
 }
